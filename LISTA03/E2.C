57d12781541bc61c3d14cb4907b0dfba3a661fcd
Arthur <kimadzn@gmail.com>
2023-09-21 14:46:28 -0300

    .

---
+++ LISTA03/E2.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+int n, soma = 0;
+printf("Digite o numero de N a serem digitados ");
+scanf("%d", &n);
+
+for (int  i = 0; i < n; i++)
+{
+    soma = 1/(pow(2,i));
+}
+    return 0;
+}
